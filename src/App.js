72555308088b877499836d3d395f126df6d9b67e
curiositypaths <curiositypaths@users.noreply.github.com>
2018-12-06 12:05:09 -0500

    Implement basic redux example

--- src/App.js
+++ src/App.js
@@ -1,28 +1,76 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { createStore } from "redux";
+import { Provider, connect } from "react-redux";
+
+// {type:"UPDATE_MESSAGE",payload:"DATA"}
+const rootReducer = function messageReducer(
+  state = "Good morning from Reducer",
+  action
+) {
+  switch (action.type) {
+    case "UPDATE_MESSAGE":
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+console.log(store.getState());
+
+const mapStateToProps = state => {
+  return {
+    message: state
+  };
+};
+
+let number = 0;
+
+const generateAction = () => {
+  return { type: "UPDATE_MESSAGE", payload: `Good morning ${++number}` };
+};
 
 class App extends Component {
+  // state = {
+  //   message: "Ciao"
+  // };
+
+  // handleChange = e => this.setState({ message: e.target.value });
+
+  componentDidMount() {
+    setInterval(() => {
+      store.dispatch(generateAction());
+    }, 2000);
+  }
+
   render() {
+    const {
+      props: { message }
+    } = this;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <input type="text" value={message} />
+
+        <p>The message is: {message}</p>
+      </>
     );
   }
 }
 
-export default App;
+const ConnectedApp = connect(mapStateToProps)(App);
+
+const Container = props => {
+  return (
+    <Provider store={store}>
+      <ConnectedApp />
+    </Provider>
+  );
+};
+
+export default Container;
